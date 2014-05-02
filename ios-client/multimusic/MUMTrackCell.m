644f7ca81db731600953171eabe6473c997eff0e
Mikkel Gravgaard <mikkel@klokke.dk>
2014-05-02 22:38:11 +0200

    layout

--- ios-client/multimusic/MUMTrackCell.m
+++ ios-client/multimusic/MUMTrackCell.m
@@ -6,16 +6,48 @@
 #import "MUMTrackCell.h"
 #import "LocalTrack.h"
 #import "MUM.h"
-
+@interface MUMTrackCell ()
+@property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UIImageView *icon;
+@end
 
 @implementation MUMTrackCell {
 
 }
 
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.icon = [UIImageView new];
+        [self.contentView addSubview:self.icon];
+        [self.icon mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.icon.superview).offset(5);
+            make.top.equalTo(self.icon.superview).offset(5);
+            make.bottom.equalTo(self.icon.superview).offset(-5);
+            make.height.equalTo(self.icon.mas_width);
+        }];
+
+        self.label = [UILabel new];
+        self.label.font = [UIFont systemFontOfSize:9];
+        [self.contentView addSubview:self.label];
+
+        [self.label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(self.label.superview);
+            make.left.equalTo(self.icon.mas_right).offset(10);
+        }];
+
+
+    }
+
+    return self;
+}
+
+
 - (void)configure:(id <MUMTrack>)track {
-    self.imageView.image = [[track class] sourceImage];
-    self.textLabel.text = track.trackDescription;
+    self.icon.image = [[track class] sourceImage];
+    self.label.text = track.trackDescription;
 
 }
+
 
 @end
