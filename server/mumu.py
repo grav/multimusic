2603bf877c3ad0eaf4c1fd9a56e36802a6223e74
Mikkel Gravgaard <mikkel@klokke.dk>
2014-06-29 14:26:35 +0200

    cleaned up json a bit by removing null values

--- server/mumu.py
+++ server/mumu.py
@@ -23,7 +23,7 @@ def metadata(path):
     filenames = [path+'/'+filename for filename in os.listdir(path)]
     filenames = filter(lambda x: x.endswith('mp3'),filenames)
     metas = [kaa.metadata.parse(filename) for filename in filenames]
-    metas_filtered = [{k : meta[k] for k in ['artist', 'title','album']} for meta in metas]
+    metas_filtered = [{k : meta[k] for k in ['artist', 'title','album'] if meta[k]} for meta in metas]
     filenames_dict = [{"filename" : filename} for filename in filenames]
     return [dict(a.items()+b.items()) for a,b in zip(filenames_dict,metas_filtered)]
         
