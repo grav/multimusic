24881ade7b66148265dbb9ee3ef9e51c2c315dcc
Mikkel Gravgaard <mikkel@klokke.dk>
2014-12-11 00:11:01 +0100

    .. and don't add media to path

--- server/mumu.py
+++ server/mumu.py
@@ -22,8 +22,7 @@ def serve(media,port):
     reactor.run()
 
 def metadata(path):
-    filenames = [path+'/'+filename for filename in os.listdir(path)]
-    filenames = filter(lambda x: x.endswith('mp3'),filenames)
+    filenames = filter(lambda x: x.endswith('mp3'),os.listdir(path))
     metas = [mutagen.File(filename,easy=True) for filename in filenames]
     metas_filtered = [{k : meta[k][0] for k in ['artist', 'title','album'] if meta[k]} for meta in metas]
     filenames_dict = [{"filename" : filename} for filename in filenames]
