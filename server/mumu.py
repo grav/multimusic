2014733ee04582ab0552f2ba286e25dedbe42be9
Mikkel Gravgaard <mikkel@klokke.dk>
2014-12-11 00:14:34 +0100

    ok, just revert to old hardcoded path

--- server/mumu.py
+++ server/mumu.py
@@ -15,29 +15,29 @@ LIBRARY = 'library.json'
 # default server port
 PORT = 8000
 
-def serve(media,port):
-    root = static.File(media)
+def serve(port):
+    root = static.File('.')
     reactor.listenTCP(port, server.Site(root))
     print "serving at port", port
     reactor.run()
 
 def metadata(path):
+    filenames = [path+'/'+filename for filename in ]
     filenames = filter(lambda x: x.endswith('mp3'),os.listdir(path))
     metas = [mutagen.File(filename,easy=True) for filename in filenames]
     metas_filtered = [{k : meta[k][0] for k in ['artist', 'title','album'] if meta[k]} for meta in metas]
     filenames_dict = [{"filename" : filename} for filename in filenames]
     return [dict(a.items()+b.items()) for a,b in zip(filenames_dict,metas_filtered)]
         
-media = sys.argv[1] if len(sys.argv) >= 2 else MEDIA
 port = int(sys.argv[2]) if len(sys.argv) >= 3 else PORT
 
-d = {"tracks":metadata(media)}
+d = {"tracks":metadata(MEDIA)}
 
 jsn = json.dumps(d)
 
-with open(media+"/"+LIBRARY, "w") as f:
+with open(LIBRARY, "w") as f:
     f.write(jsn)
 
 print "Loaded %d files into library." % len(d["tracks"])
 
-serve(media,port)
+serve(port)
