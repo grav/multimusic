0cc9e90097fc5e93bc10bfe5db4c4fefd3d0b33e
Mikkel Gravgaard <mikkel@klokke.dk>
2015-01-28 21:59:07 +0000

    allow more extensions

--- server/mumu.py
+++ server/mumu.py
@@ -21,9 +21,13 @@ def serve(port):
     print "serving at port", port
     reactor.run()
 
+def is_valid_audio(filename):
+    valid_exts = ["mp3","mp4","m4a"];
+    return reduce(lambda a,b: a or filename.endswith(b),valid_exts,False)
+
 def metadata(path):
     filenames = [path+'/'+filename for filename in os.listdir(path)]
-    filenames = filter(lambda x: x.endswith('mp3'),filenames)
+    filenames = filter(lambda x: is_valid_audio(x),filenames)
     metas = [mutagen.File(filename,easy=True) for filename in filenames]
     metas_filtered = [{k : meta[k][0] for k in ['artist', 'title','album'] if meta[k]} for meta in metas]
     filenames_dict = [{"filename" : filename} for filename in filenames]
